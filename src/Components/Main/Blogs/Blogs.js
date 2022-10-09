12e3c487740ecfe0adcb11ea1fd42931206e46f7
Saqib Ahmad <you@example.com>
2022-10-09 18:59:23 +0600

    Blog-2 is added

--- src/Components/Main/Blogs/Blogs.js
+++ src/Components/Main/Blogs/Blogs.js
@@ -15,7 +15,38 @@ const Blogs = () => {
                     As a result the hole page is not reloaded and it saves a lot of time. It is a short description how reacts work.
                 </p>
             </div>
-            
+            <div className='blog'>
+                <h2 style={{ textAlign: "center" }}><i>Blog-2</i></h2>
+                <h3>What are the differences Between Props and state in react?</h3>
+                <p>Answer: <br /> <br />
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Props</th>
+                                <th>State</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>The Data is passed from one component to another.</td>
+                                <td>The Data is passed within the component only.</td>
+                            </tr>
+                            <tr>
+                                <td>It is Immutable (cannot be modified).</td>
+                                <td>It is Mutable ( can be modified).</td>
+                            </tr>
+                            <tr>
+                                <td>Props can be used with state and functional components.</td>
+                                <td>State can be used only with the state components/class component (Before 16.0).</td>
+                            </tr>
+                            <tr>
+                                <td>Props are read-only.</td>
+                                <td>State is both read and write.</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </p>
+            </div>
             
         </section>
     );
